4c287e97b74f67ba75c4561e241f811e08908132
James Ebentier <jebentier@invoca.com>
2021-02-01 13:23:27 +0100

    Initial commit

---
+++ lib/escalate.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "escalate/version"
+
+module Escalate
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/escalate/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Escalate
+  VERSION = "0.1.0"
+end
